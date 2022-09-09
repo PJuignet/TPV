66b20cfdd7a2f196b74128ded134cf4b43d9da4b
EC2 Default User <ec2-user@ip-10-0-0-93.eu-west-3.compute.internal>
2022-09-09 14:58:10 +0000

    v2

--- orchestrator.sh
+++ orchestrator.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
-
-echo -n "Infra Name : "
-read INFRA_NAME
-
-cp -r TEMPLATE DEPLOYED/${INFRA_NAME}
+INFRA_NAME=$1
+KEY_NAME=$2
+TEMPLATE_NAME=$3
+cp -r TEMPLATE/${TEMPLATE_NAME} DEPLOYED/${TEMPLATE_NAME}/${INFRA_NAME}
 cd DEPLOYED/${INFRA_NAME}
-
 sed -i "s|<##INFRA_NAME##>|${INFRA_NAME}|g" *
+sed -i "s|<##KEY_NAME##>|${KEY_NAME}|g" *
+terraform init && terraform apply -auto-approve
+IP=$(cat temp_ip)
 
-terraform init
-terraform apply -auto-approve
-
+NB_SPACE=$(( 16 - $(echo ${IP} | wc -c) ))
+SPACES=""
+I=0
+while [ $I -lt $NB_SPACE ]
+do
+        SPACES=${SPACES}" "
+        I=$(( $I + 1 ))
+done
 cat << EOF
 ############################################################
 ############################################################
 ########                                           #########
 ########                                           #########
-########    Web App Available at : $(cat temp_ip)   #########
+########    Web App Available at : ${IP}${SPACES}#########
 ########                                           #########
 ########                                           #########
 ############################################################
 ############################################################
 EOF
-
 rm temp_ip
 exit
